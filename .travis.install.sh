99284007601210b308ba782c0cab3e9d5c2eddf1
Tom Yearke <tyearke@buffalo.edu>
2017-06-02 16:47:01 -0400

    Added npm lock files

--- .travis.install.sh
+++ .travis.install.sh
@@ -43,4 +43,9 @@ install_pear_dependencies
 source ~/.nvm/nvm.sh
 nvm install "$NODE_VERSION"
 nvm use "$NODE_VERSION"
+
+echo "Updating npm..."
+npm update -g npm
+
+echo "Installing npm dependencies..."
 npm install
