6813a1a728a3f7250f8ce3df51166f30092853da
Benjamin D. Plessinger <ben@plessinger.us>
2020-01-29 09:30:16 -0500

    return user supplied jobname in search

--- classes/DataWarehouse/Query/SUPREMM/RawData.php
+++ classes/DataWarehouse/Query/SUPREMM/RawData.php
@@ -64,6 +64,9 @@ class RawData extends \DataWarehouse\Query\Query
         $this->addField(new TableField($factTable, "end_time_ts"));
         $this->addField(new TableField($factTable, "cpu_user"));
 
+        // This is used by Integrations and not currently shown on the XDMoD interface
+        $this->addField(new TableField($factTable, 'name', 'job_name'));
+
         $this->addTable( $joblistTable );
         $this->addTable( $factTable );
 
