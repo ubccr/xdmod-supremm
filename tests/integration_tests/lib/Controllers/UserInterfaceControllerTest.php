52acc7d1500ebfdec5e8bd925985163d918ba992
Benjamin D. Plessinger <ben@plessinger.us>
2020-02-12 11:27:03 -0500

    add more comprehensive tests

--- tests/integration_tests/lib/Controllers/UserInterfaceControllerTest.php
+++ tests/integration_tests/lib/Controllers/UserInterfaceControllerTest.php
@@ -10,6 +10,7 @@ class UserInterfaceControllerTest extends \PHPUnit_Framework_TestCase
      */
     public function testGetMenus()
     {
+        $TEST_ARTIFACT_PATH = "../artifacts/integration/expected/";
         $helper = new \TestHarness\XdmodTestHelper();
         $helper->authenticate('cd');
 
@@ -28,14 +29,34 @@ class UserInterfaceControllerTest extends \PHPUnit_Framework_TestCase
         $data = $response[0];
 
         $realms = array();
+        $realmGroupBys = array();
         foreach ($data as $menuitem) {
             if (isset($menuitem['realm'])) {
                 if (!isset($realms[$menuitem['realm']])) {
                     $realms[$menuitem['realm']] = 0;
                 }
                 $realms[$menuitem['realm']] += 1;
+                if ($menuitem['realm'] == 'SUPREMM'){
+                    $groupByName = 'group_by_' . $menuitem['realm']  . '_' . $menuitem['group_by'];
+                    $gbParams = array(
+                        'operation' => 'get_menus',
+                        'node' => $groupByName,
+                        'group_by' => $menuitem['group_by'],
+                        'public_user' => false,
+                        'query_group' => 'tg_usage',
+                        'category' => $menuitem['realm']
+                    );
+                    $gbResponse = $helper->post('/controllers/user_interface.php', null, $gbParams);
+                    $realmGroupBys[$groupByName] = $gbResponse[0];
+                }
             }
         }
+        if (getenv('GENERATE_ARTIFACTS') === '1') {
+            file_put_contents($TEST_ARTIFACT_PATH . 'ui-getmenus-SUPREMM.json', json_encode($realmGroupBys, JSON_PRETTY_PRINT));
+            $this->markTestSkipped('Generated test artifacts.');
+        }
+        $expected = json_decode(file_get_contents(realpath($TEST_ARTIFACT_PATH . 'ui-getmenus-SUPREMM.json')), true);
+        $this->assertEquals($expected,$realmGroupBys);
 
         $this->assertArrayHasKey('SUPREMM', $realms);
         $this->assertArrayNotHasKey('JobEfficiency', $realms);
