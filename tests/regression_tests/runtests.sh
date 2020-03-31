ed587ea5aa7d06a74c2615eff4815c41793e8d02
Joseph P. White <jpwhite4@buffalo.edu>
2020-03-31 09:06:35 -0400

    Update as per review comments

--- tests/regression_tests/runtests.sh
+++ tests/regression_tests/runtests.sh
@@ -38,16 +38,14 @@ export REG_TEST_BASE="/../../../../../xdmod-supremm/tests/artifacts/regression/c
 
 if [ "$REG_TEST_ALL" == "1" ]; then
     set +e
-    REG_TEST_USER_ROLE=cd $phpunit $CD ./lib/Controllers/UsageExplorerSupremmTest.php
-    $phpunit --filter MetricExplorer .
+    REG_TEST_USER_ROLE=cd $phpunit $CD .
 
     #REG_TEST_USER_ROLE=usr $phpunit $REGUSER ./lib/Controllers/UsageExplorerSupremmTest.php
     #REG_TEST_USER_ROLE=pi $phpunit $PI ./lib/Controllers/UsageExplorerSupremmTest.php
     #REG_TEST_USER_ROLE=cs $phpunit $CS ./lib/Controllers/UsageExplorerSupremmTest.php
     #$phpunit $PUB ./lib/Controllers/UsageExplorerSupremmTest.php
 else
-    $phpunit --filter MetricExplorer . & mepid=$!
-    REG_TEST_USER_ROLE=cd $phpunit $CD ./lib/Controllers/UsageExplorerSupremmTest.php & cdpid=$!
+    REG_TEST_USER_ROLE=cd $phpunit $CD . & cdpid=$!
 
     #REG_TEST_USER_ROLE=usr $phpunit $REGUSER ./lib/Controllers/UsageExplorerSupremmTest.php & usrpid=$!
     #REG_TEST_USER_ROLE=pi $phpunit $PI ./lib/Controllers/UsageExplorerSupremmTest.php & pipid=$!
@@ -55,7 +53,7 @@ else
     #$phpunit $PUB ./lib/Controllers/UsageExplorerSupremmTest.php & pubpid=$!
 
     EXIT_STATUS=0
-    for pid in $mepid $usrpid $pipid $cdpid $cspid $pubpid;
+    for pid in $usrpid $pipid $cdpid $cspid $pubpid;
     do
         wait "$pid"
         if [ "$?" -ne "0" ];
