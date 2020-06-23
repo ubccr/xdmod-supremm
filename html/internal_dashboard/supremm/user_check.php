1844a8c482b938049bf161dab7a304f7f73ea7c6
Jeffrey T. Palmer <jtpalmer@buffalo.edu>
2020-06-23 07:09:25 -0400

    Remove legacy code

--- html/internal_dashboard/supremm/user_check.php
+++ html/internal_dashboard/supremm/user_check.php
@@ -1,7 +1,7 @@
 <?php
 
    require_once dirname(__FILE__).'/../../../configuration/linker.php';
-   
+
    @session_start();
 
    $user = NULL;
@@ -18,7 +18,7 @@
            }
        }
    }
-      
+
    if (!isset($user) || $user === NULL ) {
       // There is an issue with the account (most likely deleted while the user was logged in, and the user refreshed the entire site)
       session_destroy();
@@ -26,21 +26,12 @@
       exit;
    }
 
-   // --------------------------------------  
-   
+   // --------------------------------------
+
    function denyWithMessage($message) {
-   
-      $referer = isset($_POST['direct_to']) ? $_POST['direct_to'] : $_SERVER['SCRIPT_NAME'];
       $reject_response = $message;
-   
+
       include 'splash.php';
       exit;
-      
+
    }
-   
-   if( isset($_POST['direct_to']) ) {
-       error_log("POST direct to is " . $_POST['direct_to'] );
-       header("Location: ".$_POST['direct_to'] );
-       exit;
-   }
-?>
