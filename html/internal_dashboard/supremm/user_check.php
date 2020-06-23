6c8d2b05b0f7f13dff255fb7f94b45c617a2e9de
Jeffrey T. Palmer <jtpalmer@buffalo.edu>
2020-06-23 07:14:49 -0400

    Adjust white space

--- html/internal_dashboard/supremm/user_check.php
+++ html/internal_dashboard/supremm/user_check.php
@@ -1,37 +1,35 @@
 <?php
 
-   require_once dirname(__FILE__).'/../../../configuration/linker.php';
+require_once dirname(__FILE__) . '/../../../configuration/linker.php';
 
-   @session_start();
+@session_start();
 
-   $user = NULL;
+$user = null;
 
-   try {
-       $user = \xd_security\getLoggedInUser();
-   }
-   catch( Exception $e) {
-       if (isset($_POST['xdmod_username']) && isset($_POST['xdmod_password'])) {
-           $user = XDUser::authenticate($_POST['xdmod_username'], $_POST['xdmod_password']);
-           if($user != NULL ) {
-               // call record login to ensure token data is present in the session.
-               $token = \XDSessionManager::recordLogin($user);
-           }
-       }
-   }
+try {
+    $user = \xd_security\getLoggedInUser();
+} catch (Exception $e) {
+    if (isset($_POST['xdmod_username']) && isset($_POST['xdmod_password'])) {
+        $user = XDUser::authenticate($_POST['xdmod_username'], $_POST['xdmod_password']);
+        if ($user != null ) {
+            // call record login to ensure token data is present in the session.
+            $token = \XDSessionManager::recordLogin($user);
+        }
+    }
+}
 
-   if (!isset($user) || $user === NULL ) {
-      // There is an issue with the account (most likely deleted while the user was logged in, and the user refreshed the entire site)
-      session_destroy();
-      denyWithMessage('');
-      exit;
-   }
+if (!isset($user) || $user === null) {
+    // There is an issue with the account (most likely deleted while the user was logged in, and the user refreshed the entire site)
+    session_destroy();
+    denyWithMessage('');
+    exit;
+}
 
-   // --------------------------------------
+// --------------------------------------
 
-   function denyWithMessage($message) {
-      $reject_response = $message;
-
-      include 'splash.php';
-      exit;
-
-   }
+function denyWithMessage($message)
+{
+    $reject_response = $message;
+    include 'splash.php';
+    exit;
+}
