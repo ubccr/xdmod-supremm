fa776eef512d339b70ab59aaab3a63dc222b1c9d
hltaylor26 <hltaylor@buffalo.edu>
2021-04-28 10:04:22 -0400

    Added new tab to display Job Efficiency information.

---
+++ html/gui/js/Efficiency.js
@@ -0,0 +1,31 @@
+/**
+ * XDMoD.Module.Efficiency
+ *
+ */
+
+XDMoD.Module.Efficiency = Ext.extend(XDMoD.PortalModule,  {
+
+    title: 'Efficiency',   
+    module_id: 'efficiency',
+    usesToolbar: true,
+    toolbarItems: {
+       printButton: true,
+    },
+    initComponent: function () {
+ 
+       var mainArea = new Ext.Panel({
+          region: 'center',
+          html: 'This is my new module'
+       });//mainArea
+
+       Ext.apply(this, {
+          items: [
+             mainArea   
+          ]  
+       });//Ext.apply
+ 
+       XDMoD.Module.Efficiency.superclass.initComponent.apply(this, arguments);
+         
+    },//initComponent
+     
+ });//XDMoD.Module.Efficiency
