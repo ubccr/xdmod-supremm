fe135b52303ce6f494121b4e2afac2f6537b8434
hltaylor26 <hltaylor@buffalo.edu>
2021-05-04 13:57:51 -0400

    Style changes

--- tests/automated_tests/test/specs/efficiency.page.js
+++ tests/automated_tests/test/specs/efficiency.page.js
@@ -1,10 +1,9 @@
 class Efficiency {
-    constructor(){
+    constructor() {
         this.selectors = {
             tab: '#main_tab_panel__efficiency',
             container: '#efficiency'
-            }
+        };
     }
 }
 module.exports = new Efficiency();
-
