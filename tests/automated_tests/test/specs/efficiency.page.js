62855d5fc7f23721cf403f48e3d8abf7072920aa
hltaylor26 <hltaylor@buffalo.edu>
2021-05-04 13:45:15 -0400

    Added UI test that checks that the Efficiency tab is present when logged in.

---
+++ tests/automated_tests/test/specs/efficiency.page.js
@@ -0,0 +1,10 @@
+class Efficiency {
+    constructor(){
+        this.selectors = {
+            tab: '#main_tab_panel__efficiency',
+            container: '#efficiency'
+            }
+    }
+}
+module.exports = new Efficiency();
+
