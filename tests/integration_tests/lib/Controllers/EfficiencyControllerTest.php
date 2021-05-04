6c6b7957c69e80959112c3f9238c6a13bdd0c375
hltaylor26 <hltaylor@buffalo.edu>
2021-05-04 08:29:28 -0400

    Added integration test that checks the get_tabs endpoint for the efficiency tab

---
+++ tests/integration_tests/lib/Controllers/EfficiencyControllerTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace IntegrationTests\Controllers;
+
+class EfficiencyControllerTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Check that the efficiency tab shows when supremm module is installed. 
+     */
+    public function testGetEfficiencyTab()
+    {
+        $helper = new \TestHarness\XdmodTestHelper();
+        $helper->authenticate('cd');
+
+        $response = $helper->post('/controllers/user_interface.php', null, array('operation' => 'get_tabs', 'public_user' => 'false'));
+
+        $this->assertEquals('application/json', $response[1]['content_type']);
+        $this->assertEquals(200, $response[1]['http_code']);
+
+        $dwdata = $response[0];
+
+        $this->assertContains('efficiency', $dwdata['data'][0]['tabs']);
+    }
+}
