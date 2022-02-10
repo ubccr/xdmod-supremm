d3ba2aedc80a02313ccfe82e9b9561965347a5f7
hltaylor26 <79609783+hltaylor26@users.noreply.github.com>
2022-02-10 10:00:41 -0500

    Adds ability to have alternative help text for the histogram view. (#309)

--- html/gui/js/modules/efficiency/ScatterPlotPanel.js
+++ html/gui/js/modules/efficiency/ScatterPlotPanel.js
@@ -756,6 +756,14 @@ XDMoD.Module.Efficiency.ScatterPlotPanel = Ext.extend(Ext.Panel, {
                                 }
                             }
                         ];
+
+                        // Update help text if alternative histogram text is available
+                        if (self.config.histogram.histogramHelpText) {
+                            var helpText = Ext.getCmp('helpText');
+                            helpText.update(self.config.histogram.histogramHelpText);
+                            helpText.ownerCt.doLayout();
+                        }
+
                         self.updateDescription(chartStore);
                     }
                     self.unmask();
