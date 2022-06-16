a49f9d7f609c13cb6440f247769d5f20ecbc19b0
ryanrath <ryanrath@buffalo.edu>
2022-06-16 14:53:06 -0400

    Update classes/DataWarehouse/Query/SUPREMM/SupremmDbInterface.php
    Saying goodbye to my beloved `slashifyRegex` function.
    
    Co-authored-by: Jeffrey T. Palmer <jeffrey.t.palmer@gmail.com>

--- classes/DataWarehouse/Query/SUPREMM/SupremmDbInterface.php
+++ classes/DataWarehouse/Query/SUPREMM/SupremmDbInterface.php
@@ -174,35 +174,11 @@ class SupremmDbInterface {
     {
         switch ($this->mongoDriver) {
             case 'mongo':
-                return new \MongoRegex($this->slashifyRegex($regex));
+                return new \MongoRegex("/$regex/");
             case 'mongodb':
             default:
                 return new \MongoDB\BSON\Regex($regex);
         }
-    }
-
-    /**
-     * Inspect the provided $regex argument and add a leading or trailing slash if one is not
-     * already present, returning the possibly more slashified regex. This is specfically to be
-     * used w/ the \MongoRegex class and should be removed when we've dropped CentOS7.
-     *
-     * @param string $regex
-     * @return string
-     */
-    protected function slashifyRegex($regex)
-    {
-        $leading = substr($regex, 0, 1) === '/';
-        $trailing = substr($regex, count($regex) - 1, 1) === '/';
-
-        if (!$leading) {
-            $regex = "/$regex";
-        }
-
-        if (!$trailing) {
-            $regex = "$regex/";
-        }
-
-        return $regex;
     }
 
     /**
