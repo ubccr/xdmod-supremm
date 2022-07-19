36d057fadb78a4eb209853fe107aae0b0537784d
Ryan Rathsam <ryanrath@buffalo.edu>
2022-07-19 14:28:20 -0400

    appeasing style gods

--- assets/setup.sh
+++ assets/setup.sh
@@ -10,6 +10,6 @@ xdmod_dir="$module_dir/../../.."
 echo Installing Composer dependencies
 cd "$module_dir"
 
-# NOTE: We've added COMPOSER=composer.json while we support both el8 and el7. Once we drop support 
-# for Centos7 then we can remove `COMPOSER=composer.json`. 
+# NOTE: We've added COMPOSER=composer.json while we support both el8 and el7. Once we drop support
+# for Centos7 then we can remove `COMPOSER=composer.json`.
 COMPOSER=composer.json composer install --no-dev
