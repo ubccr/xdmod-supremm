f23945c0678190f6b411bbc63ec8158020382afc
Conner Saeli <connersa@buffalo.edu>
2023-08-31 13:52:15 -0400

    Pass QA tests

--- etl/js/config/supremm/dataset_maps/prometheus.js
+++ etl/js/config/supremm/dataset_maps/prometheus.js
@@ -205,9 +205,9 @@ module.exports = function(config) {
                 }
             },
             "datasource": {
-                formula: function(job) {
-                    if (job.summarization.hasOwnProperty("datasource")) {
-                        return this.ref(job, "summarization.datasource");
+                formula: function (job) {
+                    if (job.summarization.hasOwnProperty('datasource')) {
+                        return this.ref(job, 'summarization.datasource');
                     } else {
                         return { value: 'prometheus' };
                     }
