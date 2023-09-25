5da74b7318cc738f8f31af013bf4f579a33e8333
Andrew Stoltman <aestoltm@buffalo.edu>
2023-09-25 16:01:04 -0400

    Changed sanitize() to pass by value and minor tweaks to logic flow. Added docblock comment. Fixed static call of sanitize in gettimeseries()

--- classes/DataWarehouse/Query/SUPREMM/JobMetadata.php
+++ classes/DataWarehouse/Query/SUPREMM/JobMetadata.php
@@ -330,7 +330,7 @@ class JobMetadata implements \DataWarehouse\Query\iJobMetadata
 
         $doc['schema'] = $this->supremmDbInterface->getDocument($resource_id, 'schema', 'timeseries-' . $doc['version']);
 
-        $this->sanitize($doc);
+        $doc = self::sanitize($doc);
 
         return $doc;
     }
@@ -361,19 +361,30 @@ class JobMetadata implements \DataWarehouse\Query\iJobMetadata
         return $res;
     }
 
-    private static function sanitize(&$doc)
+    /**
+     * Makes a copy of the mongo document returned in $this->gettimeseries()
+     * with NAN values replaced with the value 0.
+     *
+     * @param array $doc Mongo document return from query
+     * 
+     * @return array $ret Mongo document sanitized of NAN values
+     */
+    private static function sanitize($doc)
     {
-        foreach ($doc as $key => $val) {
-            if (is_array($val)) {
-                self::sanitize($doc[$key]);
-            }
-            else {
-                if (is_numeric($val)) {
-                    if (is_nan($val)) {
-                        $doc[$key] = 0;
+        $ret = $doc;
+
+        if (is_array($ret)) {
+            foreach ($ret as $key => $val) {
+                if (is_array($val)) {
+                    $ret[$key] = self::sanitize($ret[$key]);
+                }
+                else {
+                    if (is_numeric($val) && is_nan($ret[$key])) {
+                       $ret[$key] = 0;
                     }
                 }
             }
         }
+        return $ret;
     }
 }
