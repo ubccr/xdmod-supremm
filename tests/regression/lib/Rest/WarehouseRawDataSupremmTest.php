7880f4a4bdd4643ad32e7f48a63d40a6dec639f0
Aaron Weeden <aaronwee@buffalo.edu>
2023-12-18 09:13:32 -0500

    Add raw data regression tests.

---
+++ tests/regression/lib/Rest/WarehouseRawDataSupremmTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace RegressionTests\Rest;
+
+use IntegrationTests\BaseTest;
+use RegressionTests\TestHarness\RegressionTestHelper;
+
+/**
+ * Test for regressions in getting raw data from the warehouse.
+ */
+class WarehouseRawDataSupremmTest extends BaseTest
+{
+    /**
+     * @var RegressionTestHelper
+     */
+    private static $helper;
+
+    /**
+     * Create the helper and authenticate.
+     */
+    public static function setUpBeforeClass()
+    {
+        self::$helper = new RegressionTestHelper();
+    }
+
+    /**
+     * Test getting raw data from the warehouse.
+     *
+     * @group warehouseRawData
+     * @dataProvider getRawDataProvider
+     */
+    public function testGetRawData($testName, $input)
+    {
+        $this->assertTrue(self::$helper->checkRawData($testName, $input, true));
+    }
+
+    public function getRawDataProvider()
+    {
+        $realmParams = [
+            'supremm' => [
+                'base' => [
+                    'start_date' => '2016-12-30',
+                    'end_date' => '2017-01-01',
+                    'realm' => 'SUPREMM'
+                ],
+                'fields_and_filters' => [
+                    'fields' => 'local_job_id,Resource,PI Group,Exit Status',
+                    'filters[exit_status]' => '2,3',
+                    'filters[fieldofscience]' => '49'
+                ]
+            ]
+        ];
+        return RegressionTestHelper::getRawDataTestParams($realmParams);
+    }
+}
