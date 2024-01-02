2e1d63686192bcead7eb212c7fbad9962822f95e
Joseph White <jpwhite4@buffalo.edu>
2024-01-02 15:53:21 -0500

    Add migration for the upgrade path and update the tests.

---
+++ configuration/etl/etl_sql.d/supremm/migration-10_5_0-11_0_0.sql
@@ -0,0 +1,8 @@
+LOCK TABLES `modw_supremm`.`application` WRITE
+//
+INSERT INTO `modw_supremm`.`application` (id, name, license_type, application_fosid, url) VALUES 
+	(226,'MOM6','permissive',4,'https:\/\/www.gfdl.noaa.gov/mom-ocean-model/'),
+	(227,'ROMS','permissive',4,'https:\/\/www.myroms.org/')
+//
+UNLOCK TABLES
+//
