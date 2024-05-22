1b5fda075086b1f358e695ad2f14bfdded7c0008
Joseph White <jpwhite4@buffalo.edu>
2024-05-22 18:17:15 -0400

    Remove hard-coded id values.

--- configuration/etl/etl_sql.d/supremm/migration-10_5_0-11_0_0.sql
+++ configuration/etl/etl_sql.d/supremm/migration-10_5_0-11_0_0.sql
@@ -1,27 +1,24 @@
-LOCK TABLES `modw_supremm`.`application` WRITE, `modw_supremm`.`job` AS j WRITE, `modw_supremm`.`executable` AS e WRITE
+LOCK TABLES `modw_supremm`.`application` AS a READ, `modw_supremm`.`job` AS j WRITE, `modw_supremm`.`executable` AS e WRITE
 //
-INSERT INTO `modw_supremm`.`application` (id, name, license_type, application_fosid, url) VALUES 
-	(226,'MOM6','permissive',4,'https:\/\/www.gfdl.noaa.gov/mom-ocean-model/'),
-	(227,'ROMS','permissive',4,'https:\/\/www.myroms.org/'),
-	(228,'NEXMD','permissive',2,'https:\/\/github.com/lanl/NEXMD'),
-	(229,'Libra','permissive',23,'https:\/\/quantum-dynamics-hub.github.io/libra/index.html'),
-	(230,'DFTB+','permissive',23,'https:\/\/dftbplus.org'),
-	(231,'CDO','permissive',36,'https:\/\/code.mpimet.mpg.de/projects/cdo')
+UPDATE `modw_supremm`.`job` j, `modw_supremm`.`executable` e, `modw_supremm`.`application` a
+SET j.application_id = a.id, e.application_id = a.id WHERE j.executable_id = e.id AND e.`application_id` = 0
+   AND e.`binary` = 'nexmd.exe' AND a.name = 'NEXMD'
 //
-UPDATE `modw_supremm`.`job` j, `modw_supremm`.`executable` e SET j.application_id = 228, e.application_id = 228
-    WHERE j.executable_id = e.id AND e.`binary` = 'nexmd.exe' AND e.`application_id` = 0
+UPDATE `modw_supremm`.`job` j, `modw_supremm`.`executable` e, `modw_supremm`.`application` a
+SET j.application_id = a.id, e.application_id = a.id WHERE j.executable_id = e.id AND e.`application_id` = 0
+   AND e.`binary` = 'libra' AND a.name = 'Libra'
 //
-UPDATE `modw_supremm`.`job` j, `modw_supremm`.`executable` e SET j.application_id = 229, e.application_id = 229
-    WHERE j.executable_id = e.id AND e.`binary` = 'libra' AND e.`application_id` = 0
+UPDATE `modw_supremm`.`job` j, `modw_supremm`.`executable` e, `modw_supremm`.`application` a
+SET j.application_id = a.id, e.application_id = a.id WHERE j.executable_id = e.id AND e.`application_id` = 0
+   AND e.`binary` = 'dftbp' AND a.name = 'DFTB+'
 //
-UPDATE `modw_supremm`.`job` j, `modw_supremm`.`executable` e SET j.application_id = 230, e.application_id = 230
-    WHERE j.executable_id = e.id AND e.`binary` = 'dftbp' AND e.`application_id` = 0
+UPDATE `modw_supremm`.`job` j, `modw_supremm`.`executable` e, `modw_supremm`.`application` a
+SET j.application_id = a.id, e.application_id = a.id WHERE j.executable_id = e.id AND e.`application_id` = 0
+   AND e.`binary` = 'dftbplus' AND a.name = 'DFTB+'
 //
-UPDATE `modw_supremm`.`job` j, `modw_supremm`.`executable` e SET j.application_id = 230, e.application_id = 230
-    WHERE j.executable_id = e.id AND e.`binary` = 'dftbplus' AND e.`application_id` = 0
-//
-UPDATE `modw_supremm`.`job` j, `modw_supremm`.`executable` e SET j.application_id = 231, e.application_id = 231
-    WHERE j.executable_id = e.id AND e.`binary` = 'cdo' AND e.`application_id` = 0
+UPDATE `modw_supremm`.`job` j, `modw_supremm`.`executable` e, `modw_supremm`.`application` a
+SET j.application_id = a.id, e.application_id = a.id WHERE j.executable_id = e.id AND e.`application_id` = 0
+   AND e.`binary` = 'cdo' AND a.name = 'CDO'
 //
 UNLOCK TABLES
 //
