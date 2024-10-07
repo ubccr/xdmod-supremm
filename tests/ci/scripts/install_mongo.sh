858ca4cf2be37e5635dd6e1826dc84f57840a3f0
Conner Saeli <connersa@buffalo.edu>
2024-10-07 11:18:20 -0400

    Update MongoDB version. Remove redundant installation line.

--- tests/ci/scripts/install_mongo.sh
+++ tests/ci/scripts/install_mongo.sh
@@ -2,17 +2,17 @@
 # Install mongo server and php extensions.
 
 
-cat << EOF > /etc/yum.repos.d/mongodb-org-3.6.repo
-[mongodb-org-3.6]
+cat << EOF > /etc/yum.repos.d/mongodb-org-4.4.repo
+[mongodb-org-4.4]
 name=MongoDB Repository
-baseurl=https://repo.mongodb.org/yum/redhat/8/mongodb-org/3.6/x86_64/
+baseurl=https://repo.mongodb.org/yum/redhat/8/mongodb-org/4.4/x86_64/
 gpgcheck=1
 enabled=1
-gpgkey=https://www.mongodb.org/static/pgp/server-3.6.asc
+gpgkey=https://www.mongodb.org/static/pgp/server-4.4.asc
 EOF
 
 dnf install -y mongodb-org php-devel
 
-pecl install mongodb-1.16.2
+yes '' | pecl install mongodb-1.19.2
 
 echo "extension=mongodb.so" > /etc/php.d/40-mongodb.ini
