e53b08cdb44c05c74d7a9ffe961eb1a760bfa0c4
Aaron Weeden <aaronwee@buffalo.edu>
2025-03-03 12:07:55 -0500

    Downgrade to 4.4.

--- tests/ci/scripts/install_mongo.sh
+++ tests/ci/scripts/install_mongo.sh
@@ -2,13 +2,13 @@
 # Install mongo server and php extensions.
 
 
-cat << EOF > /etc/yum.repos.d/mongodb-org-6.0.repo
-[mongodb-org-6.0]
+cat << EOF > /etc/yum.repos.d/mongodb-org-4.4.repo
+[mongodb-org-4.4]
 name=MongoDB Repository
-baseurl=https://repo.mongodb.org/yum/redhat/8/mongodb-org/6.0/x86_64/
+baseurl=https://repo.mongodb.org/yum/redhat/8/mongodb-org/4.4/x86_64/
 gpgcheck=1
 enabled=1
-gpgkey=https://www.mongodb.org/static/pgp/server-6.0.asc
+gpgkey=https://www.mongodb.org/static/pgp/server-4.4.asc
 EOF
 
 dnf install -y mongodb-org php-devel
