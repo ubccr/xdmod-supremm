514c8ea1f2f4cb8d4c18656dbe90bcadf17e69df
connersa <connersa@buffalo.edu>
2025-10-22 18:33:48 -0400

    Removing action to alter supremm aggregate tables.

--- configuration/etl/etl_sql.d/migrations/11.0.2-11.5.0/modw_aggregates/convert_to_innodb.sql
+++
@@ -1,7 +0,0 @@
--- These tables cannot be locked for writing while converting to InnoDB as it
--- will cause a deadlock and the migration process will time out.
-
-ALTER TABLE `modw_aggregates`.`supremmfact_by_day` ENGINE=InnoDB;
-ALTER TABLE `modw_aggregates`.`supremmfact_by_month` ENGINE=InnoDB;
-ALTER TABLE `modw_aggregates`.`supremmfact_by_quarter` ENGINE=InnoDB;
-ALTER TABLE `modw_aggregates`.`supremmfact_by_year` ENGINE=InnoDB;
